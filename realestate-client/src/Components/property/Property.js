ca01fbb629f15c06481d4ed7b96a5f30f22a1263
Zuhaib wani <zuhaib2482@gmail.com>
2022-07-30 20:31:08 +0530

    auth

--- realestate-client/src/Components/property/Property.js
+++ realestate-client/src/Components/property/Property.js
@@ -131,22 +131,7 @@ const Property = () =>{
                 <p className="head_column_nine">Action</p>
             </div>
 
-<<<<<<< HEAD
-            <div className="property_row">
-                <p className="property_column_one">PPD1109</p>
-                <p className="property_column_two"><FaImages className="image"/></p>
-                <p className="property_column_three">Plot</p>
-                <p className="property_column_four">8748959678</p>
-                <p className="property_column_five">1290</p>
-                <p className="property_column_six">03</p>
-                <p className="property_column_seven"><button className="btn">Sold</button></p>
-                <p className="property_column_eight">10</p>
-                <p className="property_column_nine"><BiShow className="show"/><BiPencil className="edit"/></p>
-            </div>
-            {/* {[...users].map((user)=>{
-=======
             {[...users].map((user)=>{
->>>>>>> main
                 return(
                     <div className="property_row">
                     <p className="property_column_one">PPD1234</p>
@@ -167,13 +152,8 @@ const Property = () =>{
                 return(
                     <div> {user.email} </div>
                 )
-<<<<<<< HEAD
-            })}
-                 */}
-=======
             })} */}
                 
->>>>>>> main
             
         </>
     )
